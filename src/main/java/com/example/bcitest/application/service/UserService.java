f55cd51b837ea027acd099d98a871525d00dc162
Alan Benavente <alan.bnvnt@icloud.com>
2025-09-12 23:30:07 -0300

    feat: save user on creation

--- src/main/java/com/example/bcitest/application/service/UserService.java
+++ src/main/java/com/example/bcitest/application/service/UserService.java
@@ -3,10 +3,20 @@ package com.example.bcitest.application.service;
 import com.example.bcitest.application.port.UserPort;
 import com.example.bcitest.domain.model.User;
 import com.example.bcitest.domain.service.UserValidationService;
+import com.example.bcitest.infrastructure.database.UserRepositoryAdapter;
+import com.example.bcitest.infrastructure.mapper.UserMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService implements UserPort {
+
+    private final UserRepositoryAdapter userRepository;
+
+    public UserService(UserRepositoryAdapter userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public User createUser(User user) {
@@ -16,7 +26,11 @@ public class UserService implements UserPort {
         if (!UserValidationService.isValidPassword(user.getPassword())) {
             throw new IllegalArgumentException("Invalid password format");
         }
-        return null;
+        Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("User already exists");
+        }
+        return userRepository.save(user);
     }
 
 }
