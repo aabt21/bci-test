eb28465effd4f4976d67cc07ce41f7f0e0b3b080
Alan Benavente <alan.bnvnt@icloud.com>
2025-09-12 22:33:39 -0300

    feat: create domain models

---
+++ src/main/java/com/example/bcitest/domain/model/Phone.java
@@ -0,0 +1,7 @@
+package com.example.bcitest.domain.model;
+
+public class Phone {
+    private Long number;
+    private Integer cityCode;
+    private String countryCode;
+}
---
+++ src/main/java/com/example/bcitest/domain/model/User.java
@@ -0,0 +1,17 @@
+package com.example.bcitest.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+    private List<Phone> phones;
+    private LocalDateTime created;
+    private LocalDateTime lastLogin;
+    private String token;
+    private boolean isActive;
+}
